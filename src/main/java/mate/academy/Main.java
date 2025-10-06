6ee3a4047b89118350508b7a9cb4e34197660555
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-10-06 16:41:36 +0200

    created Main.java AsyncRequestProcesor.java

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,11 +2,13 @@ package mate.academy;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
         // Feel free to play with AsyncRequestProcessor in this main method if you want
-        ExecutorService executor = null; // Provide implementation that fits your needs
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        // Provide implementation that fits your needs
         AsyncRequestProcessor asyncRequestProcessor = new AsyncRequestProcessor(executor);
 
         // Simulating multiple concurrent requests
@@ -16,7 +18,8 @@ public class Main {
         for (int i = 0; i < userIds.length; i++) {
             String userId = userIds[i];
             futures[i] = asyncRequestProcessor.processRequest(userId)
-                    .thenAccept(userData -> System.out.println("Processed: " + userData));
+                    .thenAccept(userData ->
+                            System.out.println("Processed: " + userData));
         }
 
         // Wait for all futures to complete
